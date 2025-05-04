5d76fe64104590fb082272bbd90f230ed2ac323d
Nastya <the.n_kocherovaaa@bk.ru>
2025-05-04 16:14:41 +0300

    update link

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -4,6 +4,7 @@ import { Preloader } from "../components/Preloader"
 import { Search } from "../components/Search"
 
 const API_KEY = process.env.REACT_APP_API_KEY;
+console.log(API_KEY)
 
 class Main extends React.Component {
     state = {
@@ -12,7 +13,7 @@ class Main extends React.Component {
     }
 
     componentDidMount(){
-        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
+        fetch(`http://www.omdbapi.com/?apikey=2051f233&s=matrix`)
         .then(response => response.json())
         .then(data => this.setState({movies: data.Search, loading: false}))
         .catch((err) => {
@@ -26,7 +27,7 @@ class Main extends React.Component {
     searchMovies = (str, type='all') => {
       this.setState({loading: true})
 
-        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}`: ''}`)
+        fetch(`http://www.omdbapi.com/?apikey=2051f233&s=${str}${type !== 'all' ? `&type=${type}`: ''}`)
         .then(response => response.text()) // Получаем как текст
         .then(text => {
           console.log('Fetched text:', text); // Логируем текстовый ответ
